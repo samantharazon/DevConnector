77b6be7d536b36e29d0574f89eec5a6bdab2632d
samantharazon <samantharazon2@gmail.com>
2022-06-13 18:43:19 -0400

    Get and Delete Post Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,7 +9,7 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 // ==================================================================
-// @route    Post api/posts
+// @route    POST api/posts
 // @desc     Create a post
 // @access   Private
 // ==================================================================
@@ -30,7 +30,7 @@ router.post(
     }
 
     try {
-      // get the user. How? enter token --> sends user ID -->  place in req.user.id 
+      // get the user. How? enter token --> sends user ID -->  place in req.user.id
       // dont send the password
       const user = await User.findById(req.user.id).select('-password');
 
@@ -55,6 +55,104 @@ router.post(
     }
   }
 );
+
+// ==================================================================
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Private
+// ==================================================================
+
+// add auth since its private
+router.get('/', auth, async (req, res) => {
+  try {
+    // finding all posts and sorting by most recent
+    const posts = await Post.find().sort({ date: -1 });
+    // return posts
+    res.json(posts);
+
+    // catch error
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// ==================================================================
+// @route    GET api/posts/:id
+// @desc     Get post by id
+// @access   Private
+// ==================================================================
+
+// add auth since its private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    // finding post by id. "req.params.id" allows us to get id from url
+    const post = await Post.findById(req.params.id);
+
+    // check if there is a post with that id
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // return post
+    res.json(post);
+
+    // catch error
+  } catch (err) {
+    console.error(err.message);
+
+    // check if a valid object id was passed in. if its equal to ObjectId, it's not a formatted ObjectId.
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
+// ==================================================================
+// @route    DELETE api/posts/:id
+// @desc     Delete a post
+// @access   Private
+// ==================================================================
+
+// add auth since its private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    // finding post by id. "req.params.id" allows us to get id from url
+    const post = await Post.findById(req.params.id);
+
+    // handle if post doesn't exist
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // check user
+    // make sure user deleting post is owner of post
+    // NOTE: req.user.id is a string. post.user is an object. SO add toString
+    // If user is not equal to user id, return error
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // delete post
+    await post.remove();
+
+    // return message
+    res.json({ msg: 'Post removed' });
+
+    // catch error
+  } catch (err) {
+    console.error(err.message);
+
+    // check if a valid object id was passed in. if its equal to ObjectId, it's not a formatted ObjectId.
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
 
 // Export
 module.exports = router;
